07381cd57a21206389fa0ddb7552c8443656d198
aot29 <alvaro@zbmath.org>
2022-02-23 14:49:07 +0100

    Tests now have coverage

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,4 +5,6 @@ Created on Thu Feb 17 19:04:46 2022
 @author: alvaro
 """
 rm -f /tests/.coverage
-cd /tests && export PYTHONPATH=${PYTHONPATH}:../src:. && py.test test_*.py --cov=../src --cov-report=
+cd /tests &&\
+export PYTHONPATH=${PYTHONPATH}:../src:. &&\
+pytest test_*.py --cov=../src --cov-report term

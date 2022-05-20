fa8f8709655590186247217ac314c7006d49699d
Eloi Ferrer <eloiferrer@protonmail.com>
2022-05-20 07:07:35 +0000

    Apply black format

--- src/mardi_importer/wikibase/WBEntity.py
+++ src/mardi_importer/wikibase/WBEntity.py
@@ -5,7 +5,8 @@ from mardi_importer.wikibase.WBAPIConnection import WBAPIConnection
 from mardi_importer.wikibase.WBMapping import get_wbs_local_id
 import configparser
 
-class WBEntity():
+
+class WBEntity:
     def __init__(self, label):
         self.label = label
         self.description = ""
@@ -13,10 +14,10 @@ class WBEntity():
 
         config = configparser.ConfigParser()
         config.sections()
-        config.read('/config/credentials.ini')
-        username = config['default']['username']
-        botpwd = config['default']['password']
-        WIKIBASE_API = config['default']['WIKIBASE_API']
+        config.read("/config/credentials.ini")
+        username = config["default"]["username"]
+        botpwd = config["default"]["password"]
+        WIKIBASE_API = config["default"]["WIKIBASE_API"]
         self.wb_connection = WBAPIConnection(username, botpwd, WIKIBASE_API)
 
     def add_description(self, description):
@@ -28,57 +29,69 @@ class WBEntity():
             qualifiers = self.process_qualifiers(qualifiers)
         if property[0:3] == "WD_":
             property = get_wbs_local_id(property[3:])
-        data_type = self.get_datatype(property)        
+        data_type = self.get_datatype(property)
         if data_type == "string" or data_type == "url":
-            statement = {'mainsnak':{
-                            'snaktype':'value', 
-                            'property':property, 
-                            'datavalue': {'type':'string', 'value': value}
-                            },
-                         'type': 'statement',
-                         'rank': 'normal'}
+            statement = {
+                "mainsnak": {
+                    "snaktype": "value",
+                    "property": property,
+                    "datavalue": {"type": "string", "value": value},
+                },
+                "type": "statement",
+                "rank": "normal",
+            }
         elif data_type == "wikibase-item":
             if value[0:3] == "WD_":
                 value = get_wbs_local_id(value[3:])
-            statement = {'mainsnak':{
-                            'snaktype':'value',
-                            'property':property,
-                            'datavalue': {'type':'wikibase-entityid', 'value': {'entity-type':'item','id':value}}
-                            },
-                        'type': 'statement',
-                        'rank': 'normal'}
+            statement = {
+                "mainsnak": {
+                    "snaktype": "value",
+                    "property": property,
+                    "datavalue": {
+                        "type": "wikibase-entityid",
+                        "value": {"entity-type": "item", "id": value},
+                    },
+                },
+                "type": "statement",
+                "rank": "normal",
+            }
         elif data_type == "time":
-            statement = {'mainsnak':{
-                            'snaktype':'value',
-                            'property':property,
-                            'datavalue': {'type':'time', 
-                                          'value': { 
-                                                "time":value,
-                                                'precision':11,
-                                                'timezone':0,
-                                                'before':0,
-                                                'after':0,
-                                                'calendarmodel':'http://www.wikidata.org/entity/Q1985727'
-                                                   }
-                                         }
-                                    },
-                        'type': 'statement',
-                        'rank': 'normal'}
+            statement = {
+                "mainsnak": {
+                    "snaktype": "value",
+                    "property": property,
+                    "datavalue": {
+                        "type": "time",
+                        "value": {
+                            "time": value,
+                            "precision": 11,
+                            "timezone": 0,
+                            "before": 0,
+                            "after": 0,
+                            "calendarmodel": "http://www.wikidata.org/entity/Q1985727",
+                        },
+                    },
+                },
+                "type": "statement",
+                "rank": "normal",
+            }
         elif data_type == "external-id":
-            statement = {'mainsnak': {
-                            'snaktype':'value',
-                            'property':property,
-                            'datavalue': {
-                                'type': "string",
-                                'value': value,
-                            },
-                            },
-                        'rank': "normal",
-                        'type': "statement"}
-        
+            statement = {
+                "mainsnak": {
+                    "snaktype": "value",
+                    "property": property,
+                    "datavalue": {
+                        "type": "string",
+                        "value": value,
+                    },
+                },
+                "rank": "normal",
+                "type": "statement",
+            }
+
         if qualifiers:
-            statement['qualifiers'] = qualifiers
-        
+            statement["qualifiers"] = qualifiers
+
         self.claims.append(statement)
         return self
 
@@ -88,13 +101,13 @@ class WBEntity():
     def update(self):
         qid = self.exists()
         data = {}
-        data['claims'] = self.claims
+        data["claims"] = self.claims
         return self.wb_connection.edit_entity(qid, data)
 
     def exists(self):
         pass
 
-    def process_qualifiers(self,qualifiers):
+    def process_qualifiers(self, qualifiers):
         qualifier_dict = {}
         for key, value in qualifiers.items():
             if key[0:3] == "WD_":
@@ -103,49 +116,56 @@ class WBEntity():
                 property_qualifier = key
             data_type_qualifier = self.get_datatype(property_qualifier)
             if data_type_qualifier == "string":
-                qualifier_dict[property_qualifier] = [{
-                    'snaktype': 'value',
-                    'property': property_qualifier,
-                    'datatype': 'string',
-                    'datavalue': {'type':'string', 'value': value}
-                }]
+                qualifier_dict[property_qualifier] = [
+                    {
+                        "snaktype": "value",
+                        "property": property_qualifier,
+                        "datatype": "string",
+                        "datavalue": {"type": "string", "value": value},
+                    }
+                ]
             elif data_type_qualifier == "wikibase-item":
                 if value[0:3] == "WD_":
                     value = get_wbs_local_id(value[3:])
-                qualifier_dict[property_qualifier] = [{
-                    'snaktype':'value',
-                    'property':property_qualifier,
-                    'datatype': 'wikibase-item',
-                    'datavalue': {'type':'wikibase-entityid', 'value': {'entity-type':'item','id':value}}
-                }]
-            elif data_type_qualifier == "time":
-                qualifier_dict[property_qualifier] = [{
-                    'snaktype': 'value',
-                    'property': property_qualifier,
-                    'datatype': 'time',
-                    'datavalue': {
-                        'value': {
-                        'time': value,
-                        'timezone': 0,
-                        'before': 0,
-                        'after': 0,
-                        'precision': 11,
-                        'calendarmodel': 'http://www.wikidata.org/entity/Q1985727'
+                qualifier_dict[property_qualifier] = [
+                    {
+                        "snaktype": "value",
+                        "property": property_qualifier,
+                        "datatype": "wikibase-item",
+                        "datavalue": {
+                            "type": "wikibase-entityid",
+                            "value": {"entity-type": "item", "id": value},
                         },
-                        'type': 'time'
                     }
-                }]
+                ]
+            elif data_type_qualifier == "time":
+                qualifier_dict[property_qualifier] = [
+                    {
+                        "snaktype": "value",
+                        "property": property_qualifier,
+                        "datatype": "time",
+                        "datavalue": {
+                            "value": {
+                                "time": value,
+                                "timezone": 0,
+                                "before": 0,
+                                "after": 0,
+                                "precision": 11,
+                                "calendarmodel": "http://www.wikidata.org/entity/Q1985727",
+                            },
+                            "type": "time",
+                        },
+                    }
+                ]
         return qualifier_dict
 
-    def get_datatype(self,property):
-        params = {
-            'action': 'wbgetentities',
-            'ids': property,
-            'props': 'datatype'
-        }
-        r1 = self.wb_connection.session.post(self.wb_connection.WIKIBASE_API, data=params)
+    def get_datatype(self, property):
+        params = {"action": "wbgetentities", "ids": property, "props": "datatype"}
+        r1 = self.wb_connection.session.post(
+            self.wb_connection.WIKIBASE_API, data=params
+        )
         r1.json = r1.json()
-        if 'entities' in r1.json.keys():
-            if len(r1.json['entities']) > 0:
-                return r1.json['entities'][property]['datatype']
+        if "entities" in r1.json.keys():
+            if len(r1.json["entities"]) > 0:
+                return r1.json["entities"][property]["datatype"]
         return None
--- src/mardi_importer/wikibase/WBProperty.py
+++ src/mardi_importer/wikibase/WBProperty.py
@@ -3,22 +3,23 @@
 
 from mardi_importer.wikibase.WBEntity import WBEntity
 
+
 class WBProperty(WBEntity):
-    def __init__(self,label):
+    def __init__(self, label):
         self.datatype = ""
-        super(WBProperty,self).__init__(label)
+        super(WBProperty, self).__init__(label)
 
     def create(self):
-        data = {'labels':{'en':{'language':'en','value':self.label}}}
+        data = {"labels": {"en": {"language": "en", "value": self.label}}}
         if len(self.description) > 0:
-            data['descriptions'] = {'en':{'language':'en','value':self.description}}
-        data['datatype'] = self.datatype
-        data['claims'] = self.claims
-        return self.wb_connection.create_entity('property',data)
+            data["descriptions"] = {"en": {"language": "en", "value": self.description}}
+        data["datatype"] = self.datatype
+        data["claims"] = self.claims
+        return self.wb_connection.create_entity("property", data)
 
     def exists(self):
-        return self.wb_connection.read_entity_by_title('property', self.label)
+        return self.wb_connection.read_entity_by_title("property", self.label)
 
-    def add_datatype(self,datatype):
+    def add_datatype(self, datatype):
         self.datatype = datatype
         return self

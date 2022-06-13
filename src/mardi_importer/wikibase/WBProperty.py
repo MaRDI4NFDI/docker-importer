58d19433b4143140f355c80fbc19d41191ed0fd4
Eloi Ferrer <eloiferrer@protonmail.com>
2022-06-13 14:06:17 +0000

    Check existence of property in SQL database

--- src/mardi_importer/wikibase/WBProperty.py
+++ src/mardi_importer/wikibase/WBProperty.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from mardi_importer.wikibase.WBEntity import WBEntity
-
+from mardi_importer.wikibase.WBMapping import wb_SQL_query
 
 class WBProperty(WBEntity):
     """Class to manage local properties in the wikibase instance.
@@ -36,6 +36,12 @@ class WBProperty(WBEntity):
           Boolean: **True** if property exists, **False** otherwise.
         """
         return self.wb_connection.read_entity_by_title("property", self.label)
+
+    def SQL_exists(self):
+        entity_number = wb_SQL_query(self.label, "property")
+        if entity_number:
+            return "P" + str(entity_number) 
+        return None
 
     def add_datatype(self, datatype):
         """Adds the data type to the instantiated property.

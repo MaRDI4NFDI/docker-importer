eb76814377d4a7ed7582eea9135a5cac5099d84c
Eloi Ferrer <eloiferrer@protonmail.com>
2023-06-09 05:49:24 +0000

    Add author pool

--- src/mardi_importer/polydb/PolyDBSource.py
+++ src/mardi_importer/polydb/PolyDBSource.py
@@ -30,60 +30,68 @@ class PolyDBSource(ADataSource):
     collections: List[Collection] = field(default_factory=list)
 
     def __post_init__(self):
-        #self.collection_list = ["Manifolds.DIM2_3",
-        #                        "Matroids.SelfDual",
-        #                        "Matroids.Small"]
         self.collection_list = ["Manifolds.DIM2_3",
                                 "Matroids.SelfDual",
-                                "Matroids.Small",
-                                "Polytopes.Combinatorial.01Polytopes",
-                                "Polytopes.Combinatorial.CombinatorialTypes",
-                                "Polytopes.Combinatorial.FacesBirkhoffPolytope",
-                                "Polytopes.Combinatorial.SmallSpheresDim4",
-                                "Polytopes.Geometric.01Polytopes",
-                                "Polytopes.Lattice.01Polytopes",
-                                "Polytopes.Lattice.ExceptionalMaximalHollow",
-                                "Polytopes.Lattice.FewLatticePoints3D",
-                                "Polytopes.Lattice.NonSpanning3D",
-                                "Polytopes.Lattice.Panoptigons",
-                                "Polytopes.Lattice.Reflexive",
-                                "Polytopes.Lattice.SmallVolume",
-                                "Polytopes.Lattice.SmoothReflexive",
-                                "Tropical.Cubics",
-                                "Tropical.Polytropes",
-                                "Tropical.QuarticCurves",
-                                "Tropical.SchlaefliFan",
-                                "Tropical.TOM"]
-        author_tuples = [('Frank Lutz', '', '', 'wd:Q51985'),
-                         ('Constantin Fischer', '', '', 'wd:Q51985'),
-                         ('Sachi Hashimoto', '0000-0002-8936-5545', '', 'wd:Q1912085'),
-                         ('Bernd Sturmfels', '0000-0002-6642-1479', '', 'wd:Q1912085'),
-                         ('Raluca Vlad', '', '', 'wd:Q49114'),
-                         ('Yoshitake Matsumoto', '', '', 'wd:Q7842'),
-                         ('Hiroshi Imai', '', '', 'wd:Q7842'),
-                         ('David Bremner', '', '', 'wd:Q1112515'),
-                         ('Oswin Aichholzer', '0000-0002-2364-0583', '', 'wd:Q689775'),
-                         ('Hiroyuki Miyata', '', '', 'wd:Q7842'),
-                         ('Sonoko Moriyama', '0000-0003-3358-7779', '', 'wd:Q1062129'),
-                         ('Komei Fukuda', '', '', 'wd:Q11942'),
-                         ('Andreas Paffenholz', '0000-0001-9718-523X', '', 'wd:Q310695'),
-                         ('Moritz Firsching', '', '', 'wd:Q153006'),
-                         ('Monica Blanco', '', '', 'wd:Q766962'),
-                         ('Francisco Santos', '0000-0003-2120-9068', '', 'wd:Q766962'),
-                         ('Ayush Kumar Tewari', '0000-0002-3494-2691', '', ''),
-                         ('Maximilian Kreuzer', '', '', ''),
-                         ('Gabriele Balletti', '0000-0002-0536-0027', '', ''),
-                         ('Andreas Kretschmer', '', '', 'wd:Q655866'),
-                         ('Benjamin Lorenz', '', '', 'wd:Q51985'),
-                         ('Mikkel Oebro', '', '', ''),
-                         ('Michael Joswig', '0000-0002-4974-9659', '', 'wd:Q51985'),
-                         ('Lars Kastner', '0000-0001-9224-7761', '', 'wd:Q51985'),
-                         ('Ngoc Tran', '', 'tran_n_3', 'wd:Q49213'),
-                         ('Alheydis Geiger', '', '', 'wd:Q153978'),
-                         ('Marta Panizzut', '0000-0001-8631-6329', '', 'wd:Q51985'),
-                         ('Silke Horn', '', '', 'wd:Q310695')]
-        self.author_pool = [Author(self.integrator, name, orcid, arxiv_id, affiliation) 
-                            for name, orcid, arxiv_id, affiliation in author_tuples]
+                                "Matroids.Small"]
+        #self.collection_list = ["Manifolds.DIM2_3",
+        #                        "Matroids.SelfDual",
+        #                        "Matroids.Small",
+        #                        "Polytopes.Combinatorial.01Polytopes",
+        #                        "Polytopes.Combinatorial.CombinatorialTypes",
+        #                        "Polytopes.Combinatorial.FacesBirkhoffPolytope",
+        #                        "Polytopes.Combinatorial.SmallSpheresDim4",
+        #                        "Polytopes.Geometric.01Polytopes",
+        #                        "Polytopes.Lattice.01Polytopes",
+        #                        "Polytopes.Lattice.ExceptionalMaximalHollow",
+        #                        "Polytopes.Lattice.FewLatticePoints3D",
+        #                        "Polytopes.Lattice.NonSpanning3D",
+        #                        "Polytopes.Lattice.Panoptigons",
+        #                        "Polytopes.Lattice.Reflexive",
+        #                        "Polytopes.Lattice.SmallVolume",
+        #                        "Polytopes.Lattice.SmoothReflexive",
+        #                        "Tropical.Cubics",
+        #                        "Tropical.Polytropes",
+        #                        "Tropical.QuarticCurves",
+        #                        "Tropical.SchlaefliFan",
+        #                        "Tropical.TOM"]
+
+        # author_tuple = (name, orcid, arxiv_id, affiliation_qid, wikidata_qid)
+        author_tuples = [('Frank Lutz', '', '', 'wd:Q51985', 'wd:Q102201447'),
+                         ('Constantin Fischer', '', '', 'wd:Q51985', ''),
+                         ('Sachi Hashimoto', '0000-0002-8936-5545', '', 'wd:Q1912085', ''),
+                         ('Bernd Sturmfels', '0000-0002-6642-1479', '', 'wd:Q1912085', 'wd:Q73000'),
+                         ('Raluca Vlad', '', '', 'wd:Q49114', ''),
+                         ('Yoshitake Matsumoto', '', '', 'wd:Q7842', ''),
+                         ('Hiroshi Imai', '', '', 'wd:Q7842', ''),
+                         ('David Bremner', '', '', 'wd:Q1112515', ''),
+                         ('Oswin Aichholzer', '0000-0002-2364-0583', '', 'wd:Q689775', 'wd:Q102333743'),
+                         ('Hiroyuki Miyata', '', '', 'wd:Q7842', ''),
+                         ('Sonoko Moriyama', '0000-0003-3358-7779', '', 'wd:Q1062129', ''),
+                         ('Komei Fukuda', '', '', 'wd:Q11942', 'wd:Q98644582'),
+                         ('Andreas Paffenholz', '0000-0001-9718-523X', '', 'wd:Q310695', 'wd:Q102307516'),
+                         ('Moritz Firsching', '', '', 'wd:Q153006', 'wd:Q102507664'),
+                         ('Monica Blanco', '', '', 'wd:Q766962', 'wd:Q102689582'),
+                         ('Francisco Santos', '0000-0003-2120-9068', '', 'wd:Q766962', 'wd:Q61677726'),
+                         ('Ayush Kumar Tewari', '0000-0002-3494-2691', '', '', ''),
+                         ('Maximilian Kreuzer', '', '', '', ''),
+                         ('Gabriele Balletti', '0000-0002-0536-0027', '', '', 'wd:Q103013875'),
+                         ('Andreas Kretschmer', '', '', 'wd:Q655866', ''),
+                         ('Benjamin Lorenz', '', '', 'wd:Q51985', ''),
+                         ('Mikkel Oebro', '', '', '', ''),
+                         ('Michael Joswig', '0000-0002-4974-9659', '', 'wd:Q51985', 'wd:Q16930854'),
+                         ('Lars Kastner', '0000-0001-9224-7761', '', 'wd:Q51985', ''),
+                         ('Ngoc Tran', '', 'tran_n_3', 'wd:Q49213', ''),
+                         ('Alheydis Geiger', '', '', 'wd:Q153978', ''),
+                         ('Marta Panizzut', '0000-0001-8631-6329', '', 'wd:Q51985', 'wd:Q102782302'),
+                         ('Silke Horn', '', '', 'wd:Q310695', 'wd:Q102398539')]
+
+        for name, orcid, arxiv_id, affiliation, qid in author_tuples:
+            author = Author(self.integrator, name, orcid, arxiv_id, affiliation)
+            if qid:
+                qid = qid.split(':')[1]
+                author.qid = self.integrator.import_entities(qid)
+            self.author_pool.append(author)
+                
 
     def setup(self):
         """Create all necessary properties for polyDB
@@ -119,17 +127,8 @@ class PolyDBSource(ADataSource):
         if self.update:
             self.get_collection_list()
         for name in self.collection_list:
-            new_col = Collection(name)
-            self.collections.append(new_col)
-
-            #for author in new_col.authors:
-            #    print(author)
-            #for contributor in new_col.authors:
-            #    print(contributor)
-            #for maintainer in new_col.authors:
-            #    print(maintainer)
-            #for el in new_col.references:
-            #    print(el)
+            print(name)
+            self.collections.append(Collection(name))
             time.sleep(3)
 
 
@@ -145,18 +144,7 @@ class PolyDBSource(ADataSource):
             for author in collection.authors:
                 self.author_pool.extend(collection.author_pool)
 
-        print(len(self.author_pool))
-        #Take into account the Wikidata QID of some authors
-        #have to be imported at post_init and assigned the QID parameter.
-        #Author.disambiguate_authors(self.author_pool)
-
-
-
-            # Get all authors
-            # Get references with 'arxiv', 'doi'
-            # Get authors from references into the pool
-            # Then disambiguate them and create tuple with QIDs
-            # Create publications & new tuple with QIDs?
+        Author.disambiguate_authors(self.author_pool)
 
 
 

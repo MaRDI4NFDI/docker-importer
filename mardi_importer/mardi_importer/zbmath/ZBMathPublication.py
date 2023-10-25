a3479a39a02bdc4d1cbb5933233d3e41894352cb
Ubuntu <larissapusch@gmx.net>
2023-10-25 14:33:02 +0200

    add zbmath de number to description

--- mardi_importer/mardi_importer/zbmath/ZBMathPublication.py
+++ mardi_importer/mardi_importer/zbmath/ZBMathPublication.py
@@ -80,7 +80,7 @@ class ZBMathPublication:
     def init_item(self):
         item = self.api.item.new()
         item.labels.set(language="en", value=self.title)
-        item.descriptions.set(language="en", value="scientific article")
+        item.descriptions.set(language="en", value=f"scientific article; zbmath de number {self.de_number}")
         return item
 
     def create(self):

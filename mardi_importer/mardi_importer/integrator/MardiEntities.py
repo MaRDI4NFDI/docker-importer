96afa678856630646464ed9eb139089906cceee2
Eloi Ferrer <eloiferrer@protonmail.com>
2023-12-11 13:21:12 +0000

    consider also aliases when getting list of QIDs

--- mardi_importer/mardi_importer/integrator/MardiEntities.py
+++ mardi_importer/mardi_importer/integrator/MardiEntities.py
@@ -17,7 +17,7 @@ class MardiItemEntity(MardiItem):
         json_data = super(MardiItemEntity, self)._get(entity_id=entity_id, **kwargs)
         return MardiItemEntity(api=self.api).from_json(json_data=json_data['entities'][entity_id])
 
-    def get_QID(self):
+    def get_QID(self, alias=False):
         """Creates a list of QID of all items in the local wikibase with the
         same label
 
@@ -29,40 +29,50 @@ class MardiItemEntity(MardiItem):
         if 'en' in self.labels.values:
             label = self.labels.values['en'].value
 
-        with self.api.engine.connect() as connection:
-            metadata = db.MetaData()
-            try:
-                wbt_item_terms = db.Table(
-                    "wbt_item_terms", metadata, autoload_with=connection
-                )
-                wbt_term_in_lang = db.Table(
-                    "wbt_term_in_lang", metadata, autoload_with=connection
-                )
-                wbt_text_in_lang = db.Table(
-                    "wbt_text_in_lang", metadata, autoload_with=connection
-                )
-                wbt_text = db.Table(
-                    "wbt_text", metadata, autoload_with=connection
-                )
-                query = (db.select(wbt_item_terms.columns.wbit_item_id)
-                        .join(wbt_term_in_lang, wbt_item_terms.columns.wbit_term_in_lang_id == wbt_term_in_lang.columns.wbtl_id)
-                        .join(wbt_text_in_lang, wbt_term_in_lang.columns.wbtl_text_in_lang_id == wbt_text_in_lang.columns.wbxl_id)
-                        .join(wbt_text, wbt_text.columns.wbx_id == wbt_text_in_lang.columns.wbxl_text_id)
-                        .where(and_(wbt_text.columns.wbx_text == bytes(label, "utf-8"), 
-                                    wbt_term_in_lang.columns.wbtl_type_id == 1,
-                                    wbt_text_in_lang.columns.wbxl_language == bytes("en", "utf-8"))))
-                results = connection.execute(query).fetchall()
-                entity_id = []
-                if results:
-                    for result in results:
-                        entity_id.append(f"Q{str(result[0])}")
+        def query_wikidata_table(field_type):
+            # field_type = 1 : Label
+            # field_type = 2 : Alias
+            # see: https://doc.wikimedia.org/Wikibase/REL1_41/php/docs_sql_wbt_type.html
+            entity_id = []
+            with self.api.engine.connect() as connection:
+                metadata = db.MetaData()
+                try:
+                    wbt_item_terms = db.Table(
+                        "wbt_item_terms", metadata, autoload_with=connection
+                    )
+                    wbt_term_in_lang = db.Table(
+                        "wbt_term_in_lang", metadata, autoload_with=connection
+                    )
+                    wbt_text_in_lang = db.Table(
+                        "wbt_text_in_lang", metadata, autoload_with=connection
+                    )
+                    wbt_text = db.Table(
+                        "wbt_text", metadata, autoload_with=connection
+                    )
+                    query = (db.select(wbt_item_terms.columns.wbit_item_id)
+                            .join(wbt_term_in_lang, wbt_item_terms.columns.wbit_term_in_lang_id == wbt_term_in_lang.columns.wbtl_id)
+                            .join(wbt_text_in_lang, wbt_term_in_lang.columns.wbtl_text_in_lang_id == wbt_text_in_lang.columns.wbxl_id)
+                            .join(wbt_text, wbt_text.columns.wbx_id == wbt_text_in_lang.columns.wbxl_text_id)
+                            .where(and_(wbt_text.columns.wbx_text == bytes(label, "utf-8"), 
+                                        wbt_term_in_lang.columns.wbtl_type_id == field_type,
+                                        wbt_text_in_lang.columns.wbxl_language == bytes("en", "utf-8"))))
+                    results = connection.execute(query).fetchall()
+                    if results:
+                        for result in results:
+                            entity_id.append(f"Q{str(result[0])}")
 
-            except Exception as e:
-                raise ImporterException(
-                    "Error attempting to read mappings from database\n{}".format(e)
-                )
+                except Exception as e:
+                    raise ImporterException(
+                        "Error attempting to read mappings from database\n{}".format(e)
+                    )
+                
+                return entity_id
             
-            return entity_id
+        entity_id = query_wikidata_table(field_type=1)
+        if alias:
+            entity_id += query_wikidata_table(field_type=2)
+        
+        return entity_id
     
 
 class MardiPropertyEntity(MardiProperty):
